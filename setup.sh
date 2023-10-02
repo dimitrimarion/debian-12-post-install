f505805401c7b402c00e636d08db7f0c86352f27
Dimitri Marion <dimitri.marion@gmail.com>
2023-10-02 21:52:33 +0200

    initial commit

---
+++ setup.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# https://wiki.debian.org/Xorg
+sudo apt install -y xorg i3
+cp -R config/i3 ~/.config
+sudo dpkg-reconfigure keyboard-configuration
+
+## TODO: configure git
+## TODO: git clone config: vim, i3 
+## TODO: show volume indicator
+#
+
+## Network
+sudo apt install -y network-manager-gnome
+
+# Sound
+sudo apt install -y pavucontrol pulseaudio pulseaudio-utils pasystray paprefs pavumeter pulseaudio-module-zeroconf
+
+# Hack nerd fonts
+sudo apt install -y curl
+curl -OL https://github.com/ryanoasis/nerd-fonts/releases/download/v3.0.2/Hack.zip
+mkdir -p ~/.local/share/fonts
+unzip Hack.zip -d ~/.local/share/fonts/
+fc-cache
+
+
+# st terminal
+sudo apt install -y xorg-dev build-essential git
+git clone https://github.com/dimitrimarion/st-flexipatch.git
+cd st-flexipatch
+sudo make clean install
+cd -
+
+# vim
+sudo apt install -y vim
+cp config/vimrc ~/.vimrc
+
+# Deactivate laptop monitor if external connected 
+# https://wiki.archlinux.org/title/xrandr #Toggle_external_monitor
+#sudo vim /usr/share/sddm/scripts/Xsetup # deactivate laptop monitor
+
+
+###
+sudo apt install tldr xsel
+
+## git clone dt wallpapers
+## 
