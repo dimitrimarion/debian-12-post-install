542048cb9665bb110c30f04067f3245f2e97c72d
Dimitri Marion <dimitri.marion@gmail.com>
2023-10-11 22:28:48 +0200

    vscode

--- install.sh
+++ install.sh
@@ -11,9 +11,6 @@ sudo dpkg-reconfigure keyboard-configuration
 # TODO: font
 # TODO: laptop power management
 # TODO: printer
-# TODO: chromium
-# TODO: vscode
-# TODO: node
 # TODO: copy file type associations
 
 ## Network
@@ -93,13 +90,23 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 source ~/.bashrc
 nvm install --lts
 
+# vscode
+sudo apt-get install -y wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+rm -f packages.microsoft.gp
+sudo apt install apt-transport-https
+sudo apt update
+sudo apt install code
+
 # Deactivate laptop monitor if external connected 
 # https://wiki.archlinux.org/title/xrandr #Toggle_external_monitor
 #sudo vim /usr/share/sddm/scripts/Xsetup # deactivate laptop monitor
 
 
 ###
-sudo apt install -y tldr xsel htop
+sudo apt install -y tldr xsel htop firefox-esr
 
 ## git clone dt wallpapers
 ## 
