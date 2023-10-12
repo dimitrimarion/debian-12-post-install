3a9006c5c3d72453f070dfadc40eb372d76ead27
Dimitri Marion <dimitri.marion@gmail.com>
2023-10-13 00:19:18 +0200

    fix node

--- install.sh
+++ install.sh
@@ -106,7 +106,7 @@ sudo apt-get install -y wget gpg
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
 sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
-rm -f packages.microsoft.gp
+rm -f packages.microsoft.gpg
 sudo apt install apt-transport-https
 sudo apt update
 sudo apt install code
@@ -129,4 +129,4 @@ rm ~/data/Pictures/wallpapers/README.md
 
 # starship
 curl -sS https://starship.rs/install.sh | sh
-cp config/.bashrc ~/.bashrc
+echo "eval "'"$(starship init bash)"'"" >> ~/.bashrc
